e3f62ce92e59e6d322d1c2f880882b35637c3152
= <pmosadeqzadeh@gmail.com>
2026-09-17 13:47:16 +0330

    reading prices from txt file

--- prices/prices.go
+++ prices/prices.go
@@ -1,11 +1,39 @@
 package prices
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 type TaxIncludedPriceJob struct {
 	TaxRate           float64
 	InputPrices       []float64
 	TaxIncludedPrices map[string]float64
+}
+
+func (job TaxIncludedPriceJob) LoadDate() {
+	file, err := os.Open("prices.txt")
+	if err != nil {
+		fmt.Println("Reading file failed!", err)
+		return
+	}
+
+	scanner := bufio.NewScanner(file)
+
+	var lines []string
+
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	err = scanner.Err()
+	if err != nil {
+		fmt.Println("Reading the file content failed!", err)
+		file.Close()
+		return
+	}
+
 }
 
 func (job TaxIncludedPriceJob) Process() {
