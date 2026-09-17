db812df1b379c44eababad2e6456a8d9f2599856
= <pmosadeqzadeh@gmail.com>
2026-09-17 13:38:54 +0330

    updated the price package to handle the log and data handling

--- cmd/main.go
+++ cmd/main.go
@@ -1,21 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"example.com/calculator/prices"
+)
 
 func main() {
-	prices := []float64{10.0, 20.0, 30.0}
 	taxRate := []float64{0.0, 0.07, 0.1, 0.15}
 
-	result := make(map[float64][]float64)
-
 	for _, tVal := range taxRate {
-		taxIncludePrice := make([]float64, len(prices))
-		for pIdx, pVal := range prices {
-			taxIncludePrice[pIdx] = pVal * (1 + tVal)
-		}
-
-		result[tVal] = taxIncludePrice
+		priceJob := prices.NewTaxIncludedPriceJob(tVal)
+		priceJob.Process()
 	}
 
-	fmt.Println(result)
 }
--- prices/prices.go
+++ prices/prices.go
@@ -1,7 +1,28 @@
 package prices
 
+import "fmt"
+
 type TaxIncludedPriceJob struct {
 	TaxRate           float64
 	InputPrices       []float64
 	TaxIncludedPrices map[string]float64
+}
+
+func (job TaxIncludedPriceJob) Process() {
+	result := make(map[string]float64)
+
+	for _, pVal := range job.InputPrices {
+		result[fmt.Sprint("%.2f", pVal)] = pVal * (1 + job.TaxRate)
+	}
+
+	fmt.Println("Processed tax rates & prices", result)
+}
+
+func NewTaxIncludedPriceJob(taxRate float64) *TaxIncludedPriceJob {
+
+	return &TaxIncludedPriceJob{
+		InputPrices: []float64{10.0, 20.0, 30.0},
+		TaxRate:     taxRate,
+	}
+
 }
